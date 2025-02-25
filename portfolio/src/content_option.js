43bc6e58e6a5e23e48f158844a88e6fc61a2cf4b
habibamohsin <uh040983@gmail.com>
2025-02-25 16:25:40 +0000

    container config done

---
+++ portfolio/src/content_option.js
@@ -0,0 +1,123 @@
+const logotext = "Delovper";
+const meta = {
+    title: "Umme-e-Habiba",
+    description: "I’m Umm-e-Habiba Mern STack Developer _ Full stack devloper,currently working at Corvit System Faisalabad",
+};
+
+const introdata = {
+    title: "I’m Umm-e-Habiba",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Full-Stack Websites",
+    },
+    description: "MERN Stack Developer | Crafting dynamic and responsive web applications with MongoDB, Express.js, React, and Node.js.",
+    your_img_url: "https://th.bing.com/th?id=OIP.hq3FB6uoE7zvSQvy5VwN1wHaFR&w=296&h=210&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'm a MERN Stack Developer with a passion for creating dynamic, responsive, and user-friendly web applications. With a strong foundation in MongoDB, Express.js, React, and Node.js, I specialize in building scalable solutions that enhance user experiences.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "corvit",
+        date: "2025",
+    },
+   
+   
+];
+
+const skills = [{
+        name: "ReactJs",
+        value: 90,
+    },
+    {
+        name: "MonoDb",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "Es6+Features",
+        value: 90,
+    },
+    {
+        name: "Node.js",
+        value: 90,
+    },
+];
+
+
+const services = [
+    {
+        title: "MERN Stack Development",
+        description: "Building dynamic, scalable, and high-performance web applications using MongoDB, Express.js, React, and Node.js.",
+    },
+    {
+        title: "Frontend Development",
+        description: "Creating responsive and user-friendly interfaces with React, Tailwind CSS, and modern UI frameworks.",
+    },
+   
+];
+
+
+const dataportfolio = [{
+        img: "https://th.bing.com/th?id=OIP.R3hWoRuwguRS2_vKGSjYjgHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2",
+        description: "User-interface of a Construction Website for a client",
+        link: "https://renoment.com/",
+    },
+    {
+        img: "https://th.bing.com/th?id=OIP.3DtQLLRrEffbyT5S-n2ysgAAAA&w=284&h=219&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2",
+        description: "Its a fullstack website.Font-Changer Tool Integration in Wordpress ,Client Work",
+        link: "https://fontschanger.com/",
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.RT5Rr_SVU8Nsx2koHkC4BQHaGP?rs=1&pid=ImgDetMain",
+        description: "Roof Calculator,a tool which used to calculate the roof areas",
+        link: "https://calculator-teal-eight-30.vercel.app/",
+    },
+    {
+        img: "https://th.bing.com/th?id=OIP.CL8a2q77qJrLvPIP8fSG9wHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2",
+        description: "BMW site with Chatbot",
+        link: "https://bmw-rust.vercel.app/grid",
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.sBE9zRQVo-68kKCZAPV4OgHaHa?rs=1&pid=ImgDetMain",
+        description: "Nutriwell site Userinterface",
+        link: "https://nutriwell-site.vercel.app/",
+    },
+   
+   
+   
+        
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "uh040983@gmail.com",
+    YOUR_FONE: "(+92)3057618183",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    
+    linkedin: "www.linkedin.com/in/umme-habiba-42b527276",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
